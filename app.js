306fbd8fad9427aeedaa2545c4e5a40e01197ffd
Tiago Leite <tiagoleite1405@gmail.com>
2022-11-25 22:24:17 -0300

    :sparkles: Add ejs-mate engine

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 main().catch((err) => console.log('Error: ', err));
 
@@ -16,6 +17,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
