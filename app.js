eced503897a33b32229a74c014a3a1edcc973d2f
Tiago Leite <tiagoleite1405@gmail.com>
2022-12-16 08:28:32 -0300

    :sparkles: Add express static assets  path

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
 
