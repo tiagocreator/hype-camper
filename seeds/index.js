5ad662a6afc2352c9b08870b99c368f2914e816c
Tiago Leite <tiagoleite1405@gmail.com>
2022-12-26 13:49:52 -0300

    :zap: Set default owner for campground seeds

--- seeds/index.js
+++ seeds/index.js
@@ -7,6 +7,7 @@ const { places } = require('./places');
 main().catch((err) => console.log('Error: ', err));
 
 async function main() {
+  mongoose.set('strictQuery', false);
   await mongoose.connect('mongodb://localhost:27017/hype-camper');
   console.log('Database connected');
 }
@@ -17,13 +18,15 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 100; i++) {
     const randomCities = Math.floor(Math.random() * 1000);
-    const randomPrice =  Math.floor(Math.random() * 20) + 10
+    const randomPrice = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      owner: '63a9c345eb116febd50c0b79',
       location: `${cities[randomCities].city}, ${cities[randomCities].state}`,
       title: `${randomArr(descriptors)} ${randomArr(places)}`,
       image: 'https://source.unsplash.com/collection/483251',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod, diam at laoreet suscipit, nibh elit iaculis arcu, sit amet commodo ligula sapien ut arcu. Vivamus ut convallis arcu. Donec vehicula, dolor at iaculis vestibulum, enim dolor efficitur tortor, id convallis felis odio ut erat.',
-      price: randomPrice
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod, diam at laoreet suscipit, nibh elit iaculis arcu, sit amet commodo ligula sapien ut arcu. Vivamus ut convallis arcu. Donec vehicula, dolor at iaculis vestibulum, enim dolor efficitur tortor, id convallis felis odio ut erat.',
+      price: randomPrice,
     });
     await camp.save();
   }
