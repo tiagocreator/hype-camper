1099c9ee04d1ef0e675478089a04916273fd2905
Tiago Leite <tiagoleite1405@gmail.com>
2022-12-26 16:17:38 -0300

    :safety_vest: Create verify owner of the post

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
-const isLoggedIn = (req, res, next) => {
+const Campground = require('./models/campground');
+
+module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.flash('error', 'You must be registered to add a new campground');
     return res.redirect('/login');
@@ -6,4 +8,12 @@ const isLoggedIn = (req, res, next) => {
   next();
 };
 
-module.exports = isLoggedIn;
+module.exports.verifyOwner = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground.owner.equals(req.user._id)) {
+    req.flash('error', 'You must be the owner of the post to modify it');
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
