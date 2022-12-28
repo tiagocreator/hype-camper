aad3a8fc9250803ddc67c0eccb143f842f30a658
Tiago Leite <tiagoleite1405@gmail.com>
2022-12-28 01:44:45 -0300

    ✨ Add images url and name to campground

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -11,6 +11,10 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createNewCampground = async (req, res, next) => {
   const campground = new Campground(req.body.campground);
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
   campground.owner = req.user._id;
   await campground.save();
   req.flash('success', 'Campground created successfully');
