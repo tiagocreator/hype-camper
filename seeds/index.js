e57baa13ec94de97d8b0e52e8a2dc50259ecbf07
Tiago Leite <tiagoleite1405@gmail.com>
2022-12-29 12:09:20 -0300

    🐛 Fix campground seeds images bug

--- seeds/index.js
+++ seeds/index.js
@@ -16,17 +16,34 @@ const randomArr = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 20; i++) {
     const randomCities = Math.floor(Math.random() * 1000);
     const randomPrice = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       owner: '63a9c345eb116febd50c0b79',
       location: `${cities[randomCities].city}, ${cities[randomCities].state}`,
       title: `${randomArr(descriptors)} ${randomArr(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod, diam at laoreet suscipit, nibh elit iaculis arcu, sit amet commodo ligula sapien ut arcu. Vivamus ut convallis arcu. Donec vehicula, dolor at iaculis vestibulum, enim dolor efficitur tortor, id convallis felis odio ut erat.',
       price: randomPrice,
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dzatbpzyq/image/upload/v1672281260/hype-camper/gsvua0spvtuhtb18dfyv.jpg',
+          filename: 'hype-camper/gsvua0spvtuhtb18dfyv',
+        },
+        {
+          url: 'https://res.cloudinary.com/dzatbpzyq/image/upload/v1672281260/hype-camper/ci0pqhclubxc4zgdqdfl.jpg',
+          filename: 'hype-camper/ci0pqhclubxc4zgdqdfl',
+        },
+        {
+          url: 'https://res.cloudinary.com/dzatbpzyq/image/upload/v1672281261/hype-camper/tvjdlvhrbvcujfjsabhn.jpg',
+          filename: 'hype-camper/tvjdlvhrbvcujfjsabhn',
+        },
+        {
+          url: 'https://res.cloudinary.com/dzatbpzyq/image/upload/v1672281261/hype-camper/rrn4dn6fuhypycyjdn8h.jpg',
+          filename: 'hype-camper/rrn4dn6fuhypycyjdn8h',
+        },
+      ],
     });
     await camp.save();
   }
