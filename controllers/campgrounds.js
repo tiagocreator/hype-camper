ea003d7643de2132deedd9688f2afedf10b4a3f2
Tiago Leite <tiagoleite1405@gmail.com>
2022-12-29 18:24:37 -0300

    ✨ Add delete images from database and cloudinary

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary/index');
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -53,6 +54,14 @@ module.exports.updateCampground = async (req, res) => {
     filename: f.filename,
   }));
   campground.images.push(...mapImages);
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+  }
   await campground.save();
   req.flash('success', 'Campground updated successfully');
   res.redirect(`/campgrounds/${campground._id}`);
