f91bd061806f9e6e7d34f616a8f8966eabace93b
Tiago Leite <tiagoleite1405@gmail.com>
2023-01-02 15:48:37 -0300

    ✨ Add a cluster map popup virtual to campg Schema

--- models/campground.js
+++ models/campground.js
@@ -11,41 +11,55 @@ ImageSchema.virtual('thumbnail').get(function () {
   return this.url.replace('/upload', '/upload/w_200');
 });
 
-const CampgroundSchema = new Schema({
-  title: {
-    type: String,
-  },
-  price: {
-    type: Number,
-  },
-  description: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  images: [ImageSchema],
-  reviews: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Review',
-    },
-  ],
-  geometry: {
-    type: {
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+  {
+    title: {
       type: String,
-      enum: ['Point'],
-      required: true,
     },
-    coordinates: {
-      type: [Number],
-      required: true,
+    price: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    images: [ImageSchema],
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+    geometry: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
   },
+  opts
+);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+  return `
+    <a href="/campgrounds/${
+      this._id
+    }" style="text-decoration: none; font-size: 20px; font-weight: bold">${this.title}</a>
+    <p>${this.description.substring(0, 50)}...</p>
+  `;
 });
 
 CampgroundSchema.post('findOneAndDelete', async (doc) => {
