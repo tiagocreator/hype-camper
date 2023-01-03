1c1e1786e9789ea7f12969f06b7825fdc4aa0f33
Tiago Leite <tiagoleite1405@gmail.com>
2023-01-03 01:55:48 -0300

    🔒 Secure mongo to prevent injection

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const ExpressError = require('./utilities/ExpressError');
 const passport = require('passport');
 const passportLocal = require('passport-local');
 const User = require('./models/user');
+const expressMongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -35,6 +36,7 @@ app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressMongoSanitize());
 
 const sessionConfig = {
   secret: 'test_secret',
@@ -57,6 +59,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  console.log(req.query);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
