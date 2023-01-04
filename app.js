b23fb623ae4b0dffd1a82118b6af22aaedb2e131
Tiago Leite <tiagoleite1405@gmail.com>
2023-01-03 22:09:59 -0300

    🔒 Change session cookie name

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressMongoSanitize());
 
 const sessionConfig = {
+  name: 'cookie',
   secret: 'test_secret',
   resave: false,
   saveUninitialized: true,
@@ -59,7 +60,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  console.log(req.query);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
